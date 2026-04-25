303b552fea95a8bde0c425a5efc2ee6c3836ddec
Muhammad Waleed Shahzad <waleed.shahzad911@gmail.com>
2026-04-25 15:54:01 +0200

    cleaning up

--- NLPNitro.py
+++ NLPNitro.py
@@ -6,9 +6,6 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from scipy.stats import pearsonr
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 1000)
-
 
 def loadingDataFromCSV():
     print("Loading data from train_data.csv")
@@ -35,14 +32,11 @@ def features(df):
 
 
 def calculate_score(y_true, y_pred):
-    # 1. Calculate R-squared (How close are the guesses?)
     r2 = max(0, r2_score(y_true, y_pred))
 
-    # 2. Calculate Pearson Correlation (Do they go up and down together?)
     pearson = np.abs(pearsonr(y_true, y_pred)[0])
     if np.isnan(pearson): pearson = 0
 
-    # 3. The Hackathon Formula
     return 100 * (r2 + pearson) / 2
 
 
@@ -51,37 +45,18 @@ if __name__ == "__main__":
     train_df = features(train_df)
     test_df = features(test_df)
 
-    # print("\n--- FULL TRAINING DATA VIEW ---")
-    # print(train_df.head(10))
-    #
-    # print("\n--- COLUMN SUMMARY ---")
-    # print(train_df.info())
-    # print(train_df.groupby('is_punctuation')['answer'].mean())
-
-    print("\n--- FULL TRAINING DATA VIEW ---")
     print(test_df.head(10))
-
-    # print("\n--- COLUMN SUMMARY ---")
-    # print(test_df.info())
-    # print(test_df.groupby('is_punctuation')['answer'].mean())
 
     features_list = ['word_len', 'is_punctuation', 'word_freq', 'word_log_freq', 'syllable_count', 'word_index', 'is_proper_noun']
     X = train_df[features_list]
     y = train_df['answer']
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-    print(f"I have {len(X_train)} rows to study and {len(X_val)} rows for the exam!")
-    print("Training the XGBoost brain... this might take a few seconds.")
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.01, random_state=42)
+    print("Training....")
     model = xgb.XGBRegressor(n_estimators=100, learning_rate=0.1, max_depth=6, random_state=42)
 
     model.fit(X_train, y_train)
     print("Training complete!")
 
-    val_predictions = model.predict(X_val)
-
-    final_score = calculate_score(y_val, val_predictions)
-    print(f"\n YOUR CURRENT SCORE: {final_score:.2f} / 100")
-
-    print("\nPredicting reading times for the real test data...")
     X_test_final = test_df[features_list]
     real_test_predictions = model.predict(X_test_final)
 
